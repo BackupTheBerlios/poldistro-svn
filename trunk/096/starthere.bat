b0a1bdf6ab31ca95cca5bb47750bd1bcf30e4e94
austin <austin@9d346cd0-66fc-0310-bdc5-e57197e5a75e>
2005-09-06 01:22:13 +0000

    Making a POL batch based interface for building realms, compiling, cleaning up and running POL.

---
+++ trunk/096/starthere.bat
@@ -0,0 +1,60 @@
+@ECHO OFF
+
+REM -- If a special path is needed to the batch files set it here
+SET BATCH_PATH=batchfiles\
+REM ----------
+
+GOTO :MENU()
+
+REM -- MENU FUNCTION
+:MENU()
+CLS
+ECHO StartHere.bat by Austin
+ECHO ========================
+ECHO Command        Purpose
+ECHO  [ a ] - RealmGen menu        (Realm building tools)
+ECHO  [ b ] - Ecompiler menu       (Ecompile tools)
+ECHO.
+ECHO  [ c ] - Start POL.exe        (Returns to menu on exit)
+ECHO  [ d ] - Keep POL.exe running (Restarts when it exits)
+ECHO.
+ECHO  [ x ] - Quit
+
+SET /p CMD=Command:
+
+IF "%CMD%" == "a" GOTO :REALM_GEN()
+IF "%CMD%" == "b" GOTO :ECOMPILE()
+IF "%CMD%" == "c" GOTO :POL()
+IF "%CMD%" == "d" GOTO :POL_LOOP()
+IF "%CMD%" == "x" GOTO :QUIT()
+
+ECHO.
+ECHO Invalid command.
+GOTO :RETURN_TO_MENU()
+
+REM -- RETURN_TO_MENU() FUNCTION
+:RETURN_TO_MENU()
+GOTO :MENU()
+
+REM -- REALM_GEN() FUNCTION
+:REALM_GEN()
+CALL %BATCH_PATH%realmgen.bat
+GOTO RETURN_TO_MENU()
+
+REM -- ECOMPILE() FUNCTION
+:ECOMPILE()
+CALL %BATCH_PATH%ecompile.bat
+GOTO :RETURN_TO_MENU()
+
+REM -- POL() FUNCTION
+:POL()
+CALL %BATCH_PATH%pol.bat
+GOTO :RETURN_TO_MENU()
+
+REM -- ECOMPILE() FUNCTION
+:POL_LOOP()
+CALL %BATCH_PATH%loopPOL.bat
+GOTO :RETURN_TO_MENU()
+
+REM -- QUIT FUNCTION
+:QUIT()
