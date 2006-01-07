e1da55ac3b917a62c94486816c830cdf4ba1ae5f
austin <austin@9d346cd0-66fc-0310-bdc5-e57197e5a75e>
2006-01-07 15:41:53 +0000

    Finally got around to adding Tiko's compile directory addition.

--- trunk/096/batchfiles/ecompile.bat
+++ trunk/096/batchfiles/ecompile.bat
@@ -14,16 +14,18 @@ ECHO Ecompile.bat by Austin
 ECHO ========================
 ECHO Command        Purpose
 ECHO  [ a ] - Compile a specific script.
-ECHO  [ b ] - Compile all .src scripts.
-ECHO  [ c ] - Compile all scripts and output to POL\ecompile.log
+ECHO  [ b ] - Compile a directory.
+ECHO  [ c ] - Compile all .src scripts.
+ECHO  [ d ] - Compile all scripts and output to POL\ecompile.log
 ECHO.
 ECHO  [ x ] - Back
 
 SET /p CMD=Command:
 
 IF /i "%CMD%" == "a" GOTO :COMPILE_SCRIPT()
-IF /i "%CMD%" == "b" GOTO :COMPILE_ALL_SCRIPTS()
-IF /i "%CMD%" == "c" GOTO :COMPILE_ALL_SCRIPTS_OPTXT()
+IF /i "%CMD%" == "b" GOTO :COMPILE_DIRECTORY()
+IF /i "%CMD%" == "c" GOTO :COMPILE_ALL_SCRIPTS()
+IF /i "%CMD%" == "d" GOTO :COMPILE_ALL_SCRIPTS_OPTXT()
 IF /i "%CMD%" == "x" GOTO :QUIT()
 
 ECHO.
@@ -39,6 +41,12 @@ REM -- COMPILE_SCRIPT() FUNCTION
 :COMPILE_SCRIPT()
 SET /p CMD="Path to script to compile:"
 %ECOMPILE_PATH% %CMD%
+GOTO RETURN_TO_MENU()
+
+REM -- COMPILE_DIRECTORY() FUNCTION
+:COMPILE_DIRECTORY()
+SET /p CMD="Path to DIRECTORY:"
+%ECOMPILE_PATH% -b -r %CMD%
 GOTO RETURN_TO_MENU()
 
 REM -- COMPILE_ALL_SCRIPTS() FUNCTION
