62bf4283f1cc4715f4feed39b35bddeab248b2c7
panosl <panosl@9d346cd0-66fc-0310-bdc5-e57197e5a75e>
2006-04-21 11:45:40 +0000

    data update works, but it breaks the new index

--- trunk/uomul/skills.py
+++ trunk/uomul/skills.py
@@ -1,5 +1,5 @@
 from index import Index
-from struct import unpack
+from struct import unpack, pack
 
 
 def getData(file):
@@ -15,6 +15,7 @@ class Skills(Index):
 		self.data = getData(file)
 		self.max = self.__getMax()
 		self.newentries = []
+		self.newdata = ''
 
 	def __getMax(self):
 		'''Determine the max number of skills'''
@@ -28,9 +29,9 @@ class Skills(Index):
 		if id > self.max-1:
 			#TODO Define a custom class Exception SkillError
 			raise NameError('Skill ID is out of range.')
-		skillidx = self.entries[id]
-		unpacked = unpack('b'+str(skillidx[1]-1)+'s',
-			self.data[skillidx[0]:skillidx[0]+skillidx[1]])
+		skillsidx = self.entries[id]
+		unpacked = unpack('b'+str(skillsidx[1]-1)+'s',
+			self.data[skillsidx[0]:skillsidx[0]+skillsidx[1]])
 		skill = {}
 		skill['active'] = unpacked[0]
 		skill['name'] = unpacked[1][:-1]
@@ -40,22 +41,29 @@ class Skills(Index):
 		'''Replace an already existing skill, with a new one'''
 		if len(self.newentries) == 0:
 			self.newentries = self.entries[:]
+		if len(self.newdata) == 0:
+			self.newdata = self.data[:]
 		self.newentries = [list(entry) for entry in self.newentries]
 		idxold = self.newentries.pop(id)
-		idxnew = (idxold[0], len(name), 0)
-		self.newentries.insert(id, idxnew)
+		idxnew = [idxold[0], len(name), 0]
+		idxntemp = idxnew[:]
+		idxntemp[1] += 2
+		self.newentries.insert(id, idxntemp)
 		temp = idxold[1] - idxnew[1]
 		for i in range(id+1, self.max):
 			self.newentries[i][0] += temp
-		return self.newentries
+		packed = pack('b'+str(idxnew[1])+'s', active, name) + '\x00'
+		temp = self.newdata[:idxold[0]] + packed + self.newdata[idxold[0]+idxold[1]:]
+		self.newdata = temp
+		return packed
 
 	def getSkills(self):
 		skills = [self.getSkill(skill) for skill in range(0, self.max)]
 		return skills
 
-	def writeSkills(self, entries=self.newentries, data):
-		fsock = open('Skills.idx', 'w')
-		fsock.close()
+	#def writeSkills(self, entries=self.newentries, data=self.newdata):
+	#	fsock = open('Skills.idx', 'w')
+	#	fsock.close()
 
 class SkillGrp:
 	def __init__(self, file='SkillGrp.mul'):
